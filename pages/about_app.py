4a41123f7ab53b4f7f3f3dfbf0f7453d476fadcf
Cyrille Mesue Njume <71893582+CyrilleMesue@users.noreply.github.com>
2022-03-19 12:42:41 +0300

    Update about_app.py

--- pages/about_app.py
+++ pages/about_app.py
@@ -29,4 +29,6 @@ You can also train on your own dataset provided the data is in text format where
     image = Image.open('https://github.com/CyrilleMesue/dna-seq-classifier/blob/main/machinelearning.png')
     st.image(image, caption='Machine Learning for DNA Classification')
     
+    st.markdown("Github : [https://github.com/CyrilleMesue/dna-seq-classifier](https://github.com/CyrilleMesue/dna-seq-classifier)")
+    
     st.markdown("***ENJOY THE APP***")
